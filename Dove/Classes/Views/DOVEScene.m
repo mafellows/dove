6c2e4500ac20db57afa3407ecca16cdca370ec97
Michael Fellows <michael.fellows12@gmail.com>
2014-04-11 16:15:29 -0400

    Added score and game over logic. Yet to update view contents.

--- Dove/Classes/Views/DOVEScene.m
+++ Dove/Classes/Views/DOVEScene.m
@@ -10,9 +10,24 @@
 
 @interface DOVEScene ()
 
+@property (nonatomic, assign) BOOL gameOver;
+@property (nonatomic, assign) NSInteger lives;
+@property (nonatomic, assign) NSInteger points;
+
 @end
 
 @implementation DOVEScene
+
+- (instancetype)initWithSize:(CGSize)size
+{
+    self = [super initWithSize:size];
+    if (self) {
+        self.gameOver = NO;
+        self.lives = 3;
+        self.points = 0;
+    }
+    return self;
+}
 
 - (void)didMoveToView:(SKView *)view
 {
@@ -22,15 +37,20 @@
 
 - (void)generateRandomNode
 {
-    NSArray *nodes = [self allNodes];
-    NSUInteger randomNumber = arc4random() % nodes.count;
-    self.imageNode = [nodes objectAtIndex:randomNumber];
-    [self addChild:self.imageNode];
-    
-    SKAction *fadeOut = [SKAction fadeAlphaTo:0.0f duration:2.0];
-    [self.imageNode runAction:fadeOut completion:^{
-        [self generateRandomNode];
-    }];
+    if (!self.gameOver) {
+        NSArray *nodes = [self allNodes];
+        NSUInteger randomNumber = arc4random() % nodes.count;
+        self.imageNode = [nodes objectAtIndex:randomNumber];
+        [self addChild:self.imageNode];
+        SKAction *fadeOut = [SKAction fadeAlphaTo:0.0f duration:0.1];
+        
+        [self.imageNode runAction:fadeOut completion:^{
+            sleep(1); 
+            [self generateRandomNode];
+        }];
+    } else {
+        [self resetGame];
+    }
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -39,7 +59,12 @@
     CGPoint touchLocation = [touch locationInNode:self];
     
     SKNode *node = [self nodeAtPoint:touchLocation];
-    NSLog(@"Touched this node: %@", node.name);
+    if ([node.name isEqualToString:@"good"]) {
+        self.points += 10;
+        NSLog(@"%d", self.points);
+    } else if ([node.name isEqualToString:@"bad"]) {
+        [self removeLife];
+    }
 }
 
 - (NSArray *)allNodes
@@ -54,7 +79,26 @@
     badNode.name = @"bad";
     [nodes addObject:badNode];
     
+    SKSpriteNode *nothingNode = [SKSpriteNode spriteNodeWithImageNamed:@"twitter"];
+    nothingNode.name = @"nothing";
+    [nodes addObject:nothingNode];
+    
     return nodes;
+}
+
+- (void)resetGame
+{
+    self.lives = 3;
+    self.gameOver = NO;
+}
+
+- (void)removeLife
+{
+    self.lives -= 1;
+    if (self.lives <= 0) {
+        self.gameOver = YES;
+        NSLog(@"Game Over. Here's your score: %d", self.points);
+    }
 }
 
 @end
